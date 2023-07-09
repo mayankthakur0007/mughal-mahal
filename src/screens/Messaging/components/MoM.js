d3530aaf8a6d7b29db48ebbb000eb24eefd40dda
mayankthakur0007 <mayankthakur231@gmail.com>
2023-07-10 01:08:35 +0530

    mom

--- src/screens/Messaging/components/MoM.js
+++ src/screens/Messaging/components/MoM.js
@@ -1,13 +1,25 @@
 import { StyleSheet, View, Text } from "react-native";
 import React, { Component } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import CustomSwitch from "../../../components/CustomSwitch";
 
 export default class MoM extends Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      leaveTab: 1,
+    };
+  }
       
     render(){
         return (
           <SafeAreaView style={styles.container}>
-            <Text>MoM</Text>
+            <CustomSwitch
+              selectionMode={this.state.leaveTab}
+              option1="Compose"
+              option2="View MoM"
+              onSelectSwitch={()=>{}}
+            />
           </SafeAreaView>
         );
     }
